ac5c1d0ade55d1c9c4a28022b748ea3c25670394
wangzhizhou <824219521@qq.com>
2020-11-22 23:39:37 +0800

    add light staus

--- Demo/swiftui/OrzBLEDemo/OrzBLEDemo/ContentView.swift
+++ Demo/swiftui/OrzBLEDemo/OrzBLEDemo/ContentView.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import OrzBLE
+import RxSwift
 
 struct ContentView: View {
     @State var isOpen: Bool = false
+    private let bag = DisposeBag()
+    
     var body: some View {
-        Toggle("打开BLE设备", isOn: $isOpen)
-            .padding()
+        VStack {
+            Toggle("打开BLE设备", isOn: $isOpen)
+                .padding()
+        }
+        .onAppear(perform: {
+            XMCTD01YL.shared.power.subscribe { (isPowerOn) in
+                isOpen = isPowerOn
+            }.disposed(by: bag)
+            XMCTD01YL.shared.connect()
+        })
     }
 }
 
