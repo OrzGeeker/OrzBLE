04ea09ef3836d923880ded381203169472684e0a
wangzhizhou <824219521@qq.com>
2020-11-23 09:28:54 +0800

    make some function public

--- OrzBLE/Light/LightViewModel.swift
+++ OrzBLE/Light/LightViewModel.swift
@@ -9,33 +9,36 @@ import RxSwift
 
 public final class LightViewModel: ObservableObject {
     
-    @Published var isLightOpen: Bool = false {
+    @Published public var isLightOpen: Bool = false {
         willSet(newValue) {
             model.isOpen = newValue
             updateListStatue()
         }
     }
     
+    public init() {
+        configLight()
+    }
+    
+    public func connectLight() {
+        light.connect()
+    }
+    
+
+    // MARK: 私有方法
+    @Published private var model = LightModel()
+    
+    private let bag = DisposeBag()
+    private let light = XMCTD01YL.shared
+    
+
     func configLight() {
         light.power.subscribe { (isPowerOn) in
             self.isLightOpen = isPowerOn
         }.disposed(by: bag)
     }
 
-    func connectLight() {
-        light.connect()
-    }
-    
     func updateListStatue() {
         model.isOpen ? light.powerOn() : light.powerOff()
-    }
-    
-    @Published private var model = LightModel()
-    
-    private let bag = DisposeBag()
-    private let light = XMCTD01YL.shared
-    
-    init() {
-        configLight()
     }
 }
